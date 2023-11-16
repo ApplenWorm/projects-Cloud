1ebb5433761ae98783c0640093e3fc4d1bd5df32
Emomalijon Murodzoda <45791853+scullnbones@users.noreply.github.com>
2023-11-16 19:27:56 +0000

    Added, few introductory files to 3 programming languages i know. I would program mostly only in those languages to create my side projects.

---
+++ projects-git/cpp-projects/HelloWorldProgram.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+cout<<"HELLO world!\nThis is my new C++ program.";
+return 0;
+}
---
+++ projects-git/java-projects/HelloWorld.java
@@ -0,0 +1,5 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!\nThis is my nw java program.");
+    }
+}
